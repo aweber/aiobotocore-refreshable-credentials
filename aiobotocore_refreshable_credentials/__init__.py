cef6bc920a787c77fc250fdbd475122e7af2d288
Gavin M. Roy <gavinr@aweber.com>
2021-05-18 13:16:29 -0400

    Fix packaging due to version behavior

--- aiobotocore_refreshable_credentials/__init__.py
+++ aiobotocore_refreshable_credentials/__init__.py
@@ -5,6 +5,9 @@ aiobotocore-refreshable-credentials
 """
 from aiobotocore_refreshable_credentials.session import get_session
 
+version = '1.0.2'
+
 __all__ = [
-    'get_session'
+    'get_session',
+    'version'
 ]
