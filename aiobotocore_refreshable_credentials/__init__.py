73d2a9c7c0955605e4e803bb6382a045d815a602
Gavin M. Roy <gavinr@aweber.com>
2021-06-18 14:55:51 -0400

    Update __init__.py

--- aiobotocore_refreshable_credentials/__init__.py
+++ aiobotocore_refreshable_credentials/__init__.py
@@ -5,7 +5,7 @@ aiobotocore-refreshable-credentials
 """
 from aiobotocore_refreshable_credentials.session import get_session
 
-version = '1.0.2'
+version = '1.0.3'
 
 __all__ = [
     'get_session',
